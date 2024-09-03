a61da1aa406fdc7f6eb720d8bd767832ccfbae27
Yan Azevedo <yanhenriqueazevedolima@gmail.com>
2024-09-03 15:26:02 -0300

    Trabalhando com input no python

---
+++ exemplo3.py
@@ -0,0 +1,7 @@
+nota1 = float(input("Informe a 1ª nota: "))# estamos convertendo os valores textuais em valores decimais
+nota2 = float(input("Informe a 2ª nota: "))
+nota3 = float(input("Informe a 3ª nota: "))
+
+media = (nota1+nota2+nota3)/3
+
+print(f"Sua média é {media:.2f}")#.2f significa que teremos 2 casas decimais após a vírgula
