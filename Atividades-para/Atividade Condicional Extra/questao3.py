3d62e657fd9b10d8985f2b6979034ca24f08518f
Yan Azevedo <yanhenriqueazevedolima@gmail.com>
2024-09-10 14:24:27 -0300

    Atividade condicional

--- Atividades-para/Atividade Condicional Extra/questao3.py
+++ Atividades-para/Atividade Condicional Extra/questao3.py
@@ -3,8 +3,8 @@ valor = int(input("Digite a sua idade: "))
 if valor<12:
     print(f"Você é uma criança \n")
 elif valor>=12 and valor <18:
-    print(f"Você é um adolescente \n")
+    print(f"Você é um(a) adolescente \n")
 elif valor>=18 and valor <60:
-    print(f"Você é um adulto \n")
+    print(f"Você é um(a) adulto(a) \n")
 if valor>60:
     print(f"Você é um(a) idoso(a) \n")
---
+++ Atividades-para/Atividade Condicional Extra/questao7.py
@@ -0,0 +1,7 @@
+nota = float(input("Digite a nota final do aluno: "))
+frequencia = float(input("Digite a frequência do aluno (em %): "))
+
+if nota>=7 and frequencia>=75:
+    print("O aluno está aprovado.")
+else:
+    print("O aluno está reprovado.")
